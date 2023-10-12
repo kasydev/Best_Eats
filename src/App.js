93d9a707d1940def0c9a172b1f0effd08080cce6
kasy-dev <prospercollins19@gmail.com>
2023-10-12 01:01:09 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './index.css'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import HeadlineCards from './components/HeadlineCards';
+import Foods from './components/Foods';
+import Category from './components/Category';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <HeadlineCards/> 
+      <Foods/>
+      <Category/>
+    </div>
+  );
+}
+
+export default App;                              
